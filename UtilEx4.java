a56fc3885f6dccd1b0eca8bd6578d92b3b222ad1
jychoi0918 <77179029+jychoi0918@users.noreply.github.com>
2021-01-19 17:56:35 +0900

    Add files via upload

---
+++ UtilEx4.java
@@ -0,0 +1,30 @@
+package sist.com.util;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Vector;
+
+public class UtilEx4 {
+	public List<String> utilAction1() {
+		List<String> list = new ArrayList<String>();//List´Â º¤ÅÍ
+	
+		
+		
+		return new LinkedList<String>();
+	}
+	
+	public void utilAction2() {
+		ArrayList<String>list1 = new ArrayList<>();//elementData | size
+		ArrayList<String>list2 = new ArrayList<>();//elementData | size
+		
+		
+	//	list1.add()
+	}	
+		
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
